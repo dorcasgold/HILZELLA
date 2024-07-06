b6e1b6e0c3d8f1438dbf995d7e973761e60001ee
odetayo dorcas <odetayodorcas123@gmail.com>
2024-07-06 10:48:14 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Checkout from './pages/Checkout.jsx'
+import Cart from './pages/Cart.jsx'
+import Home from './pages/Home.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='checkout' element={<Checkout />} />
+      <Route path='cart' element={<Cart />} />
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
