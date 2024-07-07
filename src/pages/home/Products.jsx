822b8037fd016756a57dbeb2c9c5eda25cfc72cf
odetayo dorcas <odetayodorcas123@gmail.com>
2024-07-07 11:10:16 +0100

    categories : Tablets

--- src/pages/home/Products.jsx
+++ src/pages/home/Products.jsx
@@ -13,6 +13,11 @@ import p4 from '../../assets/products/P4.svg'
 import p5 from '../../assets/products/P5.svg'
 import p6 from '../../assets/products/P6.svg'
 import p7 from '../../assets/products/P7.svg'
+import tb1 from '../../assets/products/Tb1.svg'
+import tb2 from '../../assets/products/Tb2.svg'
+import tb3 from '../../assets/products/Tb3.svg'
+import tb5 from '../../assets/products/Tb5.svg'
+import tb8 from '../../assets/products/Tb8.svg'
 
 function Products() {
   return (
@@ -257,7 +262,168 @@ function Products() {
           </div>
 
         </div>
+
+
+        {/* Tablets */}
+        <div className="border-b-[1px] border-b-gray-500">
+          <p className="text-2xl font-semibold py-2">Tablets</p>
+        </div>
+
+        <div className="flex w-full justify-between my-2 items-center">
+          <div>
+            <div className='w-28 group '>
+              <LazyLoadImage
+                alt="Product"
+                src={tb1}
+                effect="blur"
+                className='transition-transform duration-500 ease-in transform group-hover:scale-110'
+              />
+            </div>
+            <div className='flex gap-1 py-1'>
+              <img src={imStock} alt="imStock" className='w-4' />
+              <p className='text-sm font-medium'>Amazon Fire Hd 8 Tablet</p>
+            </div>
+            <div className=' text-center py-1'>
+              <p className=' font-semibold'>$199</p>
+              <p className='bg-black text-white px-2 py-2 cursor-pointer rounded-3xl hover:bg-orange-500 ease-in-out transition-all duration-200 '>Add to cart</p>
+            </div>
+          </div>
+          <div>
+            <div className='w-28 group'>
+              <LazyLoadImage
+                alt="Product"
+                src={tb2}
+                effect="blur"
+                className='transition-transform duration-500 ease-in transform group-hover:scale-110'
+              />
+            </div>
+            <div className='flex gap-1 py-1'>
+              <img src={imStock} alt="imStock" className='w-4' />
+              <p className='text-sm font-medium'>Apple iPad Wi-fi</p>
+            </div>
+            <div className=' text-center py-1'>
+              <p className=' font-semibold'>$199</p>
+              <p className='bg-black text-white px-[2.5rem] py-2 cursor-pointer rounded-3xl hover:bg-orange-500 ease-in-out transition-all duration-200'>Add to cart</p>
+            </div>
+          </div>
+          <div className='flex flex-col justify-center'>
+            <div className='w-28 group text-center'>
+              <LazyLoadImage
+                alt="Product"
+                src={tb3}
+                effect="blur"
+                className='transition-transform duration-500 ease-in transform group-hover:scale-110'
+              />
+            </div>
+            <div className='flex gap-1 py-1'>
+              <img src={imStock} alt="imStock" className='w-4' />
+              <p className='text-sm font-medium'>Apple iPad Pro</p>
+            </div>
+            <div className=' text-center py-1'>
+              <p className=' font-semibold'>$105</p>
+              <p className='bg-black text-white py-2 cursor-pointer rounded-3xl hover:bg-orange-500 ease-in-out transition-all duration-200 '>Add to cart</p>
+            </div>
+          </div>
+          <div>
+            <div className='w-28 group'>
+              <LazyLoadImage
+                alt="Product"
+                src={tp2}
+                effect="blur"
+                className='transition-transform duration-500 ease-in transform group-hover:scale-110'
+              />
+            </div>
+            <div className='flex gap-1 py-1'>
+              <img src={imStock} alt="imStock" className='w-4' />
+              <p className='text-sm font-medium'>10 Inch Android 9 Tablet</p>
+            </div>
+            <div className=' text-center py-1'>
+              <p className=' font-semibold'>$119</p>
+              <p className='bg-black text-white px-2 py-2 cursor-pointer rounded-3xl hover:bg-orange-500 ease-in-out transition-all duration-200 '>Add to cart</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex w-full justify-between my-2 items-center">
+          <div>
+            <div className='w-28 group '>
+              <LazyLoadImage
+                alt="Product"
+                src={tb3}
+                effect="blur"
+                className='transition-transform duration-500 ease-in transform group-hover:scale-110'
+              />
+            </div>
+            <div className='flex gap-1 py-1'>
+              <img src={imStock} alt="imStock" className='w-4' />
+              <p className='text-sm font-medium'>Iphone 15 pro max</p>
+            </div>
+            <div className=' text-center py-1'>
+              <p className=' font-semibold'>$119</p>
+              <p className='bg-black text-white px-2 py-2 cursor-pointer rounded-3xl hover:bg-orange-500 ease-in-out transition-all duration-200 '>Add to cart</p>
+            </div>
+          </div>
+          <div>
+            <div className='w-28 group'>
+              <LazyLoadImage
+                alt="Product"
+                src={tb5}
+                effect="blur"
+                className='transition-transform duration-500 ease-in transform group-hover:scale-110'
+              />
+            </div>
+            <div className='flex gap-1 py-1'>
+              <img src={imStock} alt="imStock" className='w-4' />
+              <p className='text-sm font-medium'>Nokia T20Ta-1397</p>
+            </div>
+            <div className=' text-center py-1'>
+              <p className=' font-semibold'>$119</p>
+              <p className='bg-black text-white px-2 py-2 cursor-pointer rounded-3xl hover:bg-orange-500 ease-in-out transition-all duration-200'>Add to cart</p>
+            </div>
+          </div>
+          <div className=' relative'>
+            <p className='orange text-white px-2 border-black rounded border-2 py-1 text-xs text-center absolute z-50'>NEW</p>
+            <div className='w-28 group'>
+              <LazyLoadImage
+                alt="Product"
+                src={tb8}
+                effect="blur"
+                className='transition-transform duration-500 ease-in transform group-hover:scale-110'
+              />
+            </div>
+            <div className='flex gap-1 py-1'>
+              <img src={imStock} alt="imStock" className='w-4' />
+              <p className='text-sm font-medium'>10 Inch Android 9 Tablet</p>
+            </div>
+            <div className=' text-center py-1'>
+              <p className=' font-semibold'>$119</p>
+              <p className='bg-black text-white px-2 py-2 cursor-pointer rounded-3xl hover:bg-orange-500 ease-in-out transition-all duration-200 '>Add to cart</p>
+            </div>
+          </div>
+          <div>
+            <div className='w-28 group'>
+              <LazyLoadImage
+                alt="Product"
+                src={tp3}
+                effect="blur"
+                className='transition-transform duration-500 ease-in transform group-hover:scale-110'
+              />
+            </div>
+            <div className='flex gap-1 py-1'>
+              <img src={imStock} alt="imStock" className='w-4' />
+              <p className='text-sm font-medium'>Generic Tab 14</p>
+            </div>
+            <div className=' text-center py-1'>
+              <p className=' font-semibold'>$200</p>
+              <p className='bg-black text-white px-2 py-2 cursor-pointer rounded-3xl hover:bg-orange-500 ease-in-out transition-all duration-200 '>Add to cart</p>
+            </div>
+          </div>
+
+        </div>
+
       </div>
+
+
     </div>
   )
 }
